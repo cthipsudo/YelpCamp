60a222a69343d961ab15f8cdf713143c4c280cf3
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2026-02-19 20:03:44 -0500

    added basic data validator for campgrounds route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const engine = require("ejs-mate");
+const Joi = require("joi");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 const ExpressError = require("./utils/ExpressError");
@@ -31,9 +32,22 @@ app.get("/campgrounds", async (req, res) => {
   res.render("campgrounds/index", { campgrounds });
 });
 
-app.post("/campgrounds", async (req, res) => {
-  if (!req.body.campground)
-    throw new ExpressError("Invalid Campground Data", 400);
+app.post("/campgrounds", async (req, res, next) => {
+  const campgroundSchema = Joi.object({
+    campground: Joi.object({
+      title: Joi.string().required(),
+      price: Joi.number().required().min(0),
+      image: Joi.string().required(),
+      location: Joi.string().required(),
+      description: Joi.string().required(),
+    }).required(),
+  });
+  const { error } = campgroundSchema.validate(req.body || {});
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  }
+  console.log(error);
   const campground = new Campground(req.body.campground);
   await campground.save();
   res.redirect(`/campgrounds/${campground._id}`);
