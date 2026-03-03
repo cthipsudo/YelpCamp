9ac587747ea1f3cbfd45cffcb8ce72023ffff6a6
Chanpasong Thipphakhinkeo <Chanpasong.Thipphakhinkeo@gmail.com>
2026-03-03 13:20:45 -0500

    adjust helmet config

--- app.js
+++ app.js
@@ -91,6 +91,7 @@ const styleSrcUrls = [
     "https://api.tiles.mapbox.com/",
     "https://fonts.googleapis.com/",
     "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
 ];
 const connectSrcUrls = [
     "https://api.mapbox.com/",
